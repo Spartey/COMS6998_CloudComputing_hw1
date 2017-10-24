589735cbce4d146234a7a6b7494ad95a57776588
ckj96 <ckj96123@gmail.com>
2017-10-24 16:40:06 -0400

    add templates and static folder

--- app.py
+++ app.py
@@ -2,36 +2,9 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-@app.route('/hello')
-def hello():
-    return """
-<!DOCTYPE html>
-<html>
-<head>
-<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBzE9xAESye6Kde-3hT-6B90nfwUkcS8Yw&sensor=false">
-</script>
-
-<script>
-function initialize()
-{
-    var mapProp = {
-        center:new google.maps.LatLng(27.8188024,-7.3606365),
-        zoom:2,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
-    var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
- 
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
-</head>
- 
-<body>
-<div id="googleMap" style="width:1000px;height:600px;"></div>
- 
-</body>
-</html>
-"""
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
--- twittDataBaseQuery.py
+++ twittDataBaseQuery.py
@@ -2,26 +2,29 @@ import json
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
-# Authentication for AWS ES
-AWS_ACCESS_KEY = 'AKIAIFBL2BHBX2WG6OZA'
-AWS_SECRET_KEY = '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'
-region = 'us-east-1'
-service = 'es'
+def getTwitts(wordNumber):
+	# Authentication for AWS ES
+	AWS_ACCESS_KEY = 'AKIAJ64AHWCSS5HG6BDQ'
+	AWS_SECRET_KEY = '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'
+	region = 'us-east-1'
+	service = 'es'
+	wordList = ['serendipity','AlphaGo Zero','HKUST','Columbia University','Cloud Computing','Drunken Noodles','Facebook','Eason Chan','X-man','Gotham']
+	awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, region, service)
+	host = 'search-twitterdata-lz2xqedrliqfw7l423g5uo7a34.us-east-1.es.amazonaws.com'
 
-awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, region, service)
-host = 'search-twitterdata-lz2xqedrliqfw7l423g5uo7a34.us-east-1.es.amazonaws.com'
+	es = Elasticsearch(
+    	hosts = [{'host': host, 'port': 443}],
+    	http_auth = awsauth,
+    	use_ssl = True,
+    	verify_certs = True,
+    	connection_class = RequestsHttpConnection
+	)
 
-es = Elasticsearch(
-    hosts = [{'host': host, 'port': 443}],
-    http_auth = awsauth,
-    use_ssl = True,
-    verify_certs = True,
-    connection_class = RequestsHttpConnection
-)
+	# Database querying
+	search_result = es.search(index="twitters", doc_type= wordList[wordNumber], size=500)
+	# a list of json obj
+	list_twitts = search_result['hits']['hits']
 
-# Database querying
-search_result = es.search(index="twitters", doc_type="serendipity", size=500)
-# a list of json obj
-list_twitts = search_result['hits']['hits']
-
-# a list of coordinates
+	print list_twitts[0]['_source']['user']['location']
+	# a list of coordinates
+	return list_twitts
