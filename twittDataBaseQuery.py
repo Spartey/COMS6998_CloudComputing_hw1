d2e5bc25dc9a35f94cd071bd72455eb5ee99882d
ckj96 <ckj96123@gmail.com>
2017-10-25 21:56:34 -0400

    add circle feature

--- twittDataBaseQuery.py
+++ twittDataBaseQuery.py
@@ -15,7 +15,7 @@ emoji_pattern = re.compile("["
     u"\U0001F300-\U0001F5FF"  # symbols & pictographs
     u"\U0001F680-\U0001F6FF"  # transport & map symbols
     u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    "]+", flags=re.UNICODE)
+    "]+", re.UNICODE)
 
 
 # Authentication for AWS ES
